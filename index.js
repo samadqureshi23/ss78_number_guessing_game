3eb65b93a46e1e4230816fa67eb01739cdebc080
samad <samad6666qureshi@gmail.com>
2024-04-17 22:18:37 +0500

    push my guessing number game to github

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+let roundnumber = Math.floor((Math.random() * 4) + 1);
+let userinput = await inquirer.prompt({
+    type: 'number',
+    name: 'guessedNumber',
+    message: 'guess a number between 1 -4 '
+});
+if (userinput.guessedNumber === roundnumber) {
+    console.log('congratulations! you guessed correctly');
+}
+else {
+    console.log(`sorry! try to next time. correct ans is: ${roundnumber}`);
+}
